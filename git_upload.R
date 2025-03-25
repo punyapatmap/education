6242d52084e010b60e8af3e5c2097f4dbac9ec63
punyapatmap <61130014+punyapatmap@users.noreply.github.com>
2025-03-26 03:29:35 +0700

    add speaker view

---
+++ git_upload.R
@@ -0,0 +1,14 @@
+git remote add origin https://github.com/punyapatmap/education.git
+git branch -M main
+git add --all
+git commit -m "initial commit"
+git push -u origin main
+
+
+https://htmlcolorcodes.com/ #this is the website for colour
+  
+#Theme inspiration
+https://quarto.org/docs/output-formats/html-themes.html
+
+#My inspiration github
+https://shklinkenberg.github.io/statistics-lectures/site/
